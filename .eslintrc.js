d9445677e1b5d1f919eeb72610c5b99bfa9f53f1
Junior Programmer <progphyton@gmail.com>
2023-05-07 17:44:39 +0300

    init: start html-builder task

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   'env': {
     'commonjs': true,
-    'es2022': true,
+    'es2021': true,
     'node': true
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
-    'ecmaVersion': 12
+    'ecmaVersion': 'latest'
   },
   'rules': {
     'indent': [
